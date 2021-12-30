851b9458c85cf03c71b3ab901d6dff72eddbb31a
Sabrina Drashen <sdrashen@gmail.com>
2021-12-30 11:43:48 -0300

    Added link active

--- main.js
+++ main.js
@@ -26,10 +26,10 @@ for (const link of links) {
 
 /*Mudar o header da page quando der scrol. Add sombra */
 /*Primeiro precisamos verificar se a altura do header passou do scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
 function changeHeaderWhenScroll() {
-    const header = document.querySelector('#header')
-    const navHeight = header.offsetHeight
     if (window.scrollY >= navHeight) {
         header.classList.add('scroll')
     } else {
@@ -81,20 +81,58 @@ scrollReveal.reveal(
     `, { interval: 100 }
 )
 
-/*Botão voltar para o topo*/
-/**Precisamos pegar da tela o elemento botão para que eu possa aplicar uma classe nele depois que tiver uma rolagem da tela pós um determinado número*/
+const backToTopButton = document.querySelector('.back-to-top')
+    /*Botão voltar para o topo*/
+    /**Precisamos pegar da tela o elemento botão para que eu possa aplicar uma classe nele depois que tiver uma rolagem da tela pós um determinado número*/
 function backToTop() {
-    const backToTopButton = document.querySelector('.back-to-top')
     if (window.scrollY >= 560) {
         backToTopButton.classList.add('show')
     } else {
         backToTopButton.classList.remove('show')
     }
 }
+/** Menu ativo conforme a seção sendo vista na page */
+/**Dentro do main procura todas as sections que contenham um id */
+const sections = document.querySelectorAll('main section[id]')
+    /**Essa função vai ser chamada quando o scroll estiver acontecendo e ela criaria novamente a const sections se deixasse ela dentro dessa função */
+    /**Vamos imaginar uma linha para definir o momento em que o scroll vai dectetar que a page está em determinada section, assim no menu na nav vai ficar marcado o nome da section. É preciso conferir qual o id em questão. Daí entrar a classe css "ativo" */
+function activateMenuAtCurrentSection() {
+    /**YOffSet = deslocamento do eixo Y (vertical). O "+" significa para fazer um cálculo matemático pegando todo o tamanho da janela (window.innerHeight) e dividindo ele por 8 e depois multiplicando por 4. Como chegamos nesses números? Testando, raciocinando, jogando no código para ver o que melhor funciona. */
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+        /**essa var dentro dos () depois do for poderia ser uma let também */
+    for (const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+            /**O section está se referindo a uma tag html "section", assim a função getAttribute pode entrar pra pegar os ids das sections */
+        const sectionId = section.getAttribute('id')
+            /**Aqui estamos pegando 3 infos: topo, altura e id */
+            /**Esses acima fora o checkpoint geral da page */
+            /**checkpointStart e End são as linhas imaginárias do top e do bottom que servem como limite das sections para mudança de seleção dos itens da nav */
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+            /**sectionTop + sectionHeight pega a altura completa do elemento */
+        if (checkpointStart && checkpointEnd) {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                /**"Procura o nav que ul que tenha li que tenha a que contenha o href específico(*=), isso vai se transformar em "home" mas para ser mais exato é melhor colocar o caminho todo ali */
+                .classList.add('active')
+        } else {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.remove('active')
+        }
+
+        /**Recapitulando: Todas as vezes que estiver fazendo scroll, vai entrar na function activateMenuAtCurrentSection() e vai verificar qual o checkpoint do momento, vai pegar as 3 infos já mencionadas acima e verificar se a section está no checkpoint */
+    }
+}
+
 /*Algumas mudanças para melhor organização do js */
 /*Esse evento de sombra após o scroll acontece na page inteira, então usamos o window*/
 /*Abaixo está a lógica de quando houver rolagem na página */
 window.addEventListener('scroll', function() {
     changeHeaderWhenScroll()
     backToTop()
+    activateMenuAtCurrentSection()
 })
+
+/**NOTE que algumas const estão fora da função porque a função vai ficar rodando e não vai buscar/criar a const e os elementos novamente. Nesse caso aqui essa é a lógica que melhor encaixa */
